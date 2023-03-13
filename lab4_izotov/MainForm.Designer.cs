9adca4c32e402bc5da683bafbdbc33b34a731742
Aleksandr Izotov <alexander38010@mail.ru>
2023-03-13 19:41:20 +0700

    lab4

---
+++ lab4_izotov/MainForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace lab4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.stepButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.randomButton = new System.Windows.Forms.Button();
+            this.typeSelectComboBox = new System.Windows.Forms.ComboBox();
+            this.speedTrackBar = new System.Windows.Forms.TrackBar();
+            this.stepLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPictureBox.Location = new System.Drawing.Point(13, 69);
+            this.mainPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(945, 528);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            this.mainPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseDown);
+            this.mainPictureBox.Resize += new System.EventHandler(this.mainPictureBox_Resize);
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 17;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // stepButton
+            // 
+            this.stepButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.stepButton.Location = new System.Drawing.Point(337, 20);
+            this.stepButton.Margin = new System.Windows.Forms.Padding(4);
+            this.stepButton.Name = "stepButton";
+            this.stepButton.Size = new System.Drawing.Size(100, 28);
+            this.stepButton.TabIndex = 1;
+            this.stepButton.Text = "Step";
+            this.stepButton.UseVisualStyleBackColor = true;
+            this.stepButton.Click += new System.EventHandler(this.stepButton_Click);
+            // 
+            // runButton
+            // 
+            this.runButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.runButton.Location = new System.Drawing.Point(13, 20);
+            this.runButton.Margin = new System.Windows.Forms.Padding(4);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(100, 28);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "Start";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pauseButton.Location = new System.Drawing.Point(121, 20);
+            this.pauseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(100, 28);
+            this.pauseButton.TabIndex = 3;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.stopButton.Location = new System.Drawing.Point(337, 616);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(4);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(100, 28);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clearButton.Location = new System.Drawing.Point(445, 616);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 28);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // randomButton
+            // 
+            this.randomButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.randomButton.Location = new System.Drawing.Point(229, 20);
+            this.randomButton.Margin = new System.Windows.Forms.Padding(4);
+            this.randomButton.Name = "randomButton";
+            this.randomButton.Size = new System.Drawing.Size(100, 28);
+            this.randomButton.TabIndex = 7;
+            this.randomButton.Text = "Random";
+            this.randomButton.UseVisualStyleBackColor = true;
+            this.randomButton.Click += new System.EventHandler(this.randomButton_Click);
+            // 
+            // typeSelectComboBox
+            // 
+            this.typeSelectComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.typeSelectComboBox.FormattingEnabled = true;
+            this.typeSelectComboBox.Items.AddRange(new object[] {
+            "Conway\'s Game of Life"});
+            this.typeSelectComboBox.Location = new System.Drawing.Point(727, 20);
+            this.typeSelectComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.typeSelectComboBox.Name = "typeSelectComboBox";
+            this.typeSelectComboBox.Size = new System.Drawing.Size(231, 24);
+            this.typeSelectComboBox.TabIndex = 8;
+            this.typeSelectComboBox.SelectedIndexChanged += new System.EventHandler(this.typeSelectComboBox_SelectedIndexChanged);
+            // 
+            // speedTrackBar
+            // 
+            this.speedTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.speedTrackBar.Location = new System.Drawing.Point(70, 616);
+            this.speedTrackBar.Margin = new System.Windows.Forms.Padding(4);
+            this.speedTrackBar.Maximum = 3;
+            this.speedTrackBar.Minimum = 1;
+            this.speedTrackBar.Name = "speedTrackBar";
+            this.speedTrackBar.Size = new System.Drawing.Size(241, 56);
+            this.speedTrackBar.TabIndex = 9;
+            this.speedTrackBar.Value = 1;
+            this.speedTrackBar.Scroll += new System.EventHandler(this.speedTrackBar_Scroll);
+            // 
+            // stepLabel
+            // 
+            this.stepLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stepLabel.AutoSize = true;
+            this.stepLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stepLabel.Location = new System.Drawing.Point(452, 22);
+            this.stepLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.stepLabel.Name = "stepLabel";
+            this.stepLabel.Size = new System.Drawing.Size(0, 25);
+            this.stepLabel.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 616);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Speed:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 679);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.speedTrackBar);
+            this.Controls.Add(this.typeSelectComboBox);
+            this.Controls.Add(this.randomButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.stepLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.stepButton);
+            this.Controls.Add(this.mainPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Button stepButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button randomButton;
+        private System.Windows.Forms.ComboBox typeSelectComboBox;
+        private System.Windows.Forms.TrackBar speedTrackBar;
+        private System.Windows.Forms.Label stepLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
